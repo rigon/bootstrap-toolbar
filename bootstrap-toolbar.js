cdf9b5639016df53179e113e40f07e8d49689edb
rigon <ricar.goncalves@gmail.com>
2017-03-02 21:58:07 +0000

    Style fix, license info

--- bootstrap-toolbar.js
+++ bootstrap-toolbar.js
@@ -1,164 +1,179 @@
-
+/**************************************************************************
+ * Customizable toolbars for Bootstrap - https://github.com/rigon/bootstrap-toolbar
+ * Copyright (C) 2017  rigon<ricardompgoncalves@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************/
 
 function bootstraptoolbar(htmlElement, customOptions) {
-    var self = this;        // Reference for this object
-    this.container;         // jQuery object of the selected HTML element
-    this.options = {        // List of default options
-        autoredraw: true,
-        buttons: {},
-        context: {},
-        list: [],
-        show: true,
-    };
+	var self = this;	// Reference for this object
+	this.container;		// jQuery object of the selected HTML element
+	this.options = {	// List of default options
+		autoredraw: true,
+		buttons: {},
+		context: {},
+		list: [],
+		show: true,
+	};
 
-    // Global reference for this object
-    var self_id = "bootstraptoolbar_" + String(Math.random()).substr(2);  // Create a random ID
-    eval(self_id + " = this");  // Reference for this object in a var with name self_id
-
-
-    this.addButton = function(name, button) {
-        this.options.buttons[name] = button;
-        // Redraw if set
-        if(this.options.autoredraw)
-            this.redraw();
-    }
-    this.addButtons = function(buttons) {
-        var autoredraw = this.options.autoredraw;
-        this.options.autoredraw = false;    // Disable temporarily
-        // Add buttons
-        if(typeof buttons === "object")
-            for(name in buttons)
-                this.addButton(name, buttons[name]);
-        
-        this.options.autoredraw = autoredraw;    // Restore
-        if(autoredraw)
-            this.redraw();
-    }
-
-    this.addOptions = function(newOptions) {
-        // Incompatible data type
-        if(typeof newOptions !== "object") return;
-
-        // Iterate over newOptions
-        for(var name in newOptions) {
-            var newValue = newOptions[name];
-
-            switch(name) {
-                case "buttons":
-                    // Merge buttons
-                    this.addButtons(newValue);
-                    break;
-                default:
-                    // Copy values to in options
-                    this.options[name] = newValue;
-            }
-        }
-
-    }
+	// Global reference for this object
+	var self_id = "bootstraptoolbar_" + String(Math.random()).substr(2); // Create a random ID
+	eval(self_id + " = this"); // Reference for this object in a var with name self_id
 
 
-    function createButtons(list, buttons, element, self_id) {
-        var obj;
-        var group = $("<div></div>", { class: "btn-group", role: "group" });
+	this.addButton = function (name, button) {
+		this.options.buttons[name] = button;
+		// Redraw if set
+		if (this.options.autoredraw)
+			this.redraw();
+	}
+	this.addButtons = function (buttons) {
+		var autoredraw = this.options.autoredraw;
+		this.options.autoredraw = false;		// Disable temporarily
+		// Add buttons
+		if (typeof buttons === "object")
+			for (name in buttons)
+				this.addButton(name, buttons[name]);
 
-        for(var i in list) {
-            var name = list[i];
-            var button = buttons[name];
-            console.log(name);
+		this.options.autoredraw = autoredraw;	// Restore
+		if (autoredraw)
+			this.redraw();
+	}
 
-            if(name === "space") {
-                element.append(group);
-                group = $("<div></div>", { class: "btn-group", role: "group" });
-                continue;
-            }
+	this.addOptions = function (newOptions) {
+		// Incompatible data type
+		if (typeof newOptions !== "object") return;
 
-            switch(typeof button) {
-                case "string":
-                    // Create an element with the HTML
-                    group.append($(button));
-                    break;
-                case "object":
-                    // Create button
-                    obj = $('<button type="button" class="btn btn-default"></button>');
+		// Iterate over newOptions
+		for (var name in newOptions) {
+			var newValue = newOptions[name];
 
-                    // If has icon
-                    if("icon" in button) {
-                        var icon = $("<span></span>", { class: button.icon });
-                        obj.append(icon);
-                        delete button.icon;
-                    }
-                    // If has text
-                    if("text" in button) {
-                        obj.append(" " + button.text);
-                        delete button.text;
-                    }
-                    // Convert functions to string
-                    for(attribute in button)
-                        if(typeof button[attribute] === "function")
-                            button[attribute] = "("+ button[attribute] + ")(" + self_id + ".options.context, " + self_id + ")";
-                    
-                    // Set attributes
-                    obj.attr(button);
-
-                    // Add button to the group
-                    group.append(obj);
-                    break;
-                default:
-                    throw "Wrong button type";
-            }
-        }
-
-        // Append the last group
-        element.append(group);
-    }
+			switch (name) {
+				case "buttons":
+					// Merge buttons
+					this.addButtons(newValue);
+					break;
+				default:
+					// Copy values to in options
+					this.options[name] = newValue;
+			}
+		}
+	}
 
 
-    this.redraw = function() {
-        console.log(this.options.list);
-        // List of buttons
-        var hasList = (this.options.list.length > 0);
-        var list = (hasList ? this.options.list : this.options.list);
-        for(var button in this.options.buttons) {
-            var indexBefore = list.indexOf(this.options.buttons[button].before);    // Index of before
-            var indexAfter = list.indexOf(this.options.buttons[button].after);      // Index of after
-            if(indexBefore >= 0) list.splice(indexBefore, 0, button);               // Insert before
-            else if(indexAfter >= 0) list.splice(indexAfter + 1, 0, button);        // Insert after
-            else if(!hasList && list.indexOf(button) < 0) list.push(button);        // Push if not present
-        }
+	function createButtons(list, buttons, element, self_id) {
+		var obj;
+		var group = $("<div></div>", { class: "btn-group", role: "group" });
 
-        // Clear container
-        this.container.empty();
+		for (var i in list) {
+			var name = list[i];
+			var button = buttons[name];
 
-        // Create buttons
-        createButtons(list, this.options.buttons, this.container, self_id);
-    }
+			if (name === "space") {
+				element.append(group);
+				group = $("<div></div>", { class: "btn-group", role: "group" });
+				continue;
+			}
+
+			switch (typeof button) {
+				case "string":
+					// Create an element with the HTML
+					group.append($(button));
+					break;
+				case "object":
+					// Create button
+					obj = $('<button type="button" class="btn btn-default"></button>');
+
+					// If has icon
+					if ("icon" in button) {
+						var icon = $("<span></span>", {
+							class: button.icon
+						});
+						obj.append(icon);
+						delete button.icon;
+					}
+					// If has text
+					if ("text" in button) {
+						obj.append(" " + button.text);
+						delete button.text;
+					}
+					// Convert functions to string
+					for (attribute in button)
+						if (typeof button[attribute] === "function")
+							button[attribute] = "(" + button[attribute] + ")(" + self_id + ".options.context, " + self_id + ")";
+
+					// Set attributes
+					obj.attr(button);
+
+					// Add button to the group
+					group.append(obj);
+					break;
+				default:
+					throw "Wrong button type";
+			}
+		}
+
+		// Append the last group
+		element.append(group);
+	}
 
 
-    this.create = function() {
-        this.container = $('<nav class="navbar navbar-default"></div>');
-        $(htmlElement).append(this.container);
-    }
+	this.redraw = function () {
+		// List of buttons
+		var hasList = (this.options.list.length > 0);
+		var list = (hasList ? this.options.list : this.options.list);
+		for (var button in this.options.buttons) {
+			var indexBefore = list.indexOf(this.options.buttons[button].before);	// Index of before
+			var indexAfter = list.indexOf(this.options.buttons[button].after);		// Index of after
+			if (indexBefore >= 0) list.splice(indexBefore, 0, button);				// Insert before
+			else if (indexAfter >= 0) list.splice(indexAfter + 1, 0, button);		// Insert after
+			else if (!hasList && list.indexOf(button) < 0) list.push(button);		// Push if not present
+		}
 
-    var autoredraw = this.options.autoredraw;
-    this.options.autoredraw = false;        // Disable temporarily
-    
-    this.addOptions(customOptions);
-    this.create();
+		// Clear container
+		this.container.empty();
 
-    this.options.autoredraw = autoredraw;   // Restore
-    if(autoredraw)
-        this.redraw();
+		// Create buttons
+		createButtons(list, this.options.buttons, this.container, self_id);
+	}
+
+
+	this.create = function () {
+		this.container = $('<nav class="navbar"></div>');
+		$(htmlElement).append(this.container);
+	}
+
+	var autoredraw = this.options.autoredraw;
+	this.options.autoredraw = false;		// Disable temporarily
+
+	this.addOptions(customOptions);
+	this.create();
+
+	this.options.autoredraw = autoredraw;	// Restore
+	if (autoredraw)
+		this.redraw();
 }
 
 jQuery.fn.extend({
-    toolbar: function(options) {
-        // If custom options not provided
-        if(typeof options === "undefined")
-            options = {};
+	toolbar: function (options) {
+		// If custom options not provided
+		if (typeof options === "undefined")
+			options = {};
 
-        return this.each(function () {
-            // Deep copy of options
-            return new bootstraptoolbar(this, jQuery.extend(true, {}, options));
-        });
-    }
+		return this.each(function () {
+			// Deep copy of options
+			return new bootstraptoolbar(this, jQuery.extend(true, {}, options));
+		});
+	}
 });
